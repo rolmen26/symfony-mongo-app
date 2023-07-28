5cb85e6ff2e7596101d1156da2268d7a650e495d
Rommel Soriano <rommel.soriano@funiber.org>
2023-07-28 10:34:22 -0500

    feature: implementing phpunit
    - Adding dev dependencie of phpunit
    - Composer json and lock are modified
    - Example tests are added

---
+++ tests/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        var_dump('xd');
+        $this->assertTrue(true);
+    }
+}
